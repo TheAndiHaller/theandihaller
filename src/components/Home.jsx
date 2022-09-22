7f3f4dd514f6e36ac30a1337e1220a741e3cf6ec
Andi <andi_haller@hotmail.com>
2022-09-22 19:44:54 -0400

    added bootstrap links

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Navbar from "./Navbar.jsx";
 
 export default function Home() {
   return (
-    <div className="">
-      <Link to={"/"}>Home</Link>
-      <Link to={"/projects"}>Projects</Link>
-      <Link to={"/blog"}>Blog</Link>
-      <Link to={"/fullcv"}>Full CV</Link>
-      <h1>Andi's portfolio</h1>
-    </div>
+    <Container>
+      <Navbar />
+      <Row>
+        <Col>1 of 3</Col>
+        <Col xs={6}>
+          <Link to={"/"}>Home</Link>
+          <Link to={"/projects"}>Projects</Link>
+          <Link to={"/blog"}>Blog</Link>
+          <Link to={"/fullcv"}>Full CV</Link>
+        </Col>
+        <Col>3 of 3</Col>
+      </Row>
+    </Container>
   );
 }
